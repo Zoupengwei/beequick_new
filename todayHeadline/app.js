7b2ce6418d058f26f2a4a0085f486f480a0cd18a
zpw <1163654989@qq.com>
2017-03-10 19:07:30 +0800

    add

--- todayHeadline/app.js
+++ todayHeadline/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("indexApp", ["ngRoute", "angularCSS"]);
+var app = angular.module("indexApp", ["ngRoute", "angularCSS", "ui.router"]);
 
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when("/home", {
@@ -14,9 +14,42 @@ app.config(['$routeProvider', function ($routeProvider) {
         css: "./follow/css/follow.css",
         controller: "followCtrl",
     }).when("/personal", {
-        templateUrl: "/personal.html",
+        templateUrl: "./personal/personal.html",
         css: "./personal/css/personal.css",
         controller: "personalCtrl",
     }).otherwise("/home")
 
 }])
+
+//切换底部菜单栏样式
+app.config(function ($stateProvider) {
+    var homeState = {
+        name: 'home',
+        url: '/home',
+        // templateUrl: './home/home.html'
+    }
+
+    var videoState = {
+        name: 'video',
+        url: '/video',
+        // templateUrl: './video/video.html'
+    }
+
+    var followState = {
+        name: 'follow',
+        url: '/follow',
+        // templateUrl: './follow/follow.html'
+    }
+
+    var personalState = {
+        name: 'personal',
+        url: '/personal',
+        templateUrl: './personal/personal.html'
+    }
+
+    $stateProvider.state(homeState);
+    $stateProvider.state(videoState);
+    $stateProvider.state(followState);
+    $stateProvider.state(personalState);
+
+})
---
+++ todayHeadline/follow/js/follow.js
@@ -0,0 +1,7 @@
+/**
+ * Created by 邹朋位 on 2017/3/10.
+ */
+
+app.controller("followCtrl",["$scope",function ($scope) {
+    
+}])
